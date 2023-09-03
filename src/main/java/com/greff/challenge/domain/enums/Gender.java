20bad4b7c708ec52dd76eb42bd8a40b6e913ea44
VitorGreff <vitorgreffdias@gmail.com>
2023-09-03 01:58:23 -0300

    modifying enum

--- src/main/java/com/greff/challenge/domain/enums/Gender.java
+++ src/main/java/com/greff/challenge/domain/enums/Gender.java
@@ -1,17 +1,7 @@
 package com.greff.challenge.domain.enums;
 
 public enum Gender {
-    MALE("Masculine"),
-    FEMALE("Feminine"),
-    OTHER("Other");
-
-    private final String label;
-
-    Gender(String label) {
-        this.label = label;
-    }
-
-    public String getLabel() {
-        return label;
-    }
+    MALE,
+    FEMALE,
+    OTHER;
 }
